651d72fde5fcbec09a4780dcac659a5aa0eb687d
Leda <me@leda.dev>
2023-07-13 23:13:49 +0000

    feat: remove createBrowserLoginFlow request

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -13,42 +13,16 @@ class AuthController
         return $request->user() ?? (object) [];
     }
 
-    public function login(Request $request, FrontendApi $frontendApi)
+    public function login(Request $request)
     {
-        try {
-            $returnToUrl = route(config('uom-id.auth.uom.redirects.login'));
+        $returnToUrl = route(config('uom-id.auth.uom.redirects.login'));
+        $loginRouteUrl =  config('uom-id.auth.uom.routes.login');
 
-            $frontendApi->createBrowserLoginFlow(null, null, $returnToUrl, $request->header('Cookie'));
+        $loginUrl = $request::create($loginRouteUrl)->fullUrlWithQuery([
+            'return_to' => $returnToUrl,
+        ]);
 
-            $loginUrl = $request::create(config('uom-id.auth.uom.routes.login'))->fullUrlWithQuery([
-                'return_to' => $returnToUrl,
-            ]);
-
-            return redirect($loginUrl);
-        } catch (\Ory\Kratos\Client\ApiException $err) {
-            $errorBody = json_decode($err->getResponseBody());
-
-            // Handle null error body
-            if (is_null($errorBody)) {
-                if (App::hasDebugModeEnabled()) {
-                    return response()->json(['message' => $err->getMessage()]);
-                } else {
-                    return $this->genericJsonErrorResponse();
-                }
-            }
-
-            $errorId = $errorBody->error->id;
-
-            // TODO: Complete handling all errorId cases
-            switch ($errorId) {
-                case 'session_already_available':
-                    return redirect(route(config('uom-id.auth.uom.redirects.login')));
-                case 'self_service_flow_return_to_forbidden':
-                    return response()->json(['message' => "The supplied return_to address ($returnToUrl) is not allowed."]);
-                default:
-                    return $this->genericJsonErrorResponse();
-            }
-        }
+        return redirect($loginUrl);
     }
 
     public function logout(Request $request, FrontendApi $frontendApi)
